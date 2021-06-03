da5fd3bdf6b375b8410ed7e3d90617e3dc225f78
Oleg Smirnov <sm-oleg1209@yandex.ru>
2021-06-04 01:48:52 +0300

    Refactor code & clean up Add some comments and TODOs

--- evaluation/src/main/java/org/jetbrains/research/ddtc/evaluation/EvaluationRunner.java
+++ evaluation/src/main/java/org/jetbrains/research/ddtc/evaluation/EvaluationRunner.java
@@ -1,25 +1,15 @@
 package org.jetbrains.research.ddtc.evaluation;
 
-import com.intellij.ide.impl.NewProjectUtil;
 import com.intellij.ide.impl.ProjectUtil;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ApplicationStarter;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.module.JavaModuleType;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.JavaSdk;
-import com.intellij.openapi.projectRoots.ProjectJdkTable;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.roots.ModuleRootModificationUtil;
 import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
-import com.intellij.testFramework.PsiTestUtil;
 import com.intellij.usageView.UsageInfo;
 import org.apache.commons.cli.*;
 import org.jetbrains.annotations.NonNls;
@@ -32,7 +22,6 @@ import org.jetbrains.research.ide.migration.collectors.TypeChangesInfoCollector;
 import org.jetbrains.research.utils.PsiRelatedUtils;
 
 import java.io.File;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -47,16 +36,6 @@ public class EvaluationRunner implements ApplicationStarter {
         return "evaluation";
     }
 
-    private static void setupJdk(Project project, String jdkHomeDirectory) {
-        Sdk jdk = JavaSdk.getInstance().createJdk(JDK_NAME, jdkHomeDirectory, false);
-        ProjectJdkTable.getInstance().addJdk(jdk);
-        ProjectRootManager.getInstance(project).setProjectSdk(jdk);
-        NewProjectUtil.applyJdkToProject(project, jdk);
-        Module[] modules = ModuleManager.getInstance(project).getModules();
-        for (Module module : modules) {
-            ModuleRootModificationUtil.setModuleSdk(module, jdk);
-        }
-    }
 
     @Override
     public void main(@NotNull List<String> args) {
@@ -66,18 +45,22 @@ public class EvaluationRunner implements ApplicationStarter {
             String pathToJdk = parsedArgs.getOptionValue("jdk-path");
 
             ApplicationManager.getApplication().runWriteAction(() -> {
+
+                // Traverse all subdirectories in the specified directory and create project for each of them
                 for (File projectDir : Objects.requireNonNull(pathToProjects.listFiles())) {
                     if (projectDir.isDirectory()) {
                         final Project project = ProjectUtil.openOrImport(projectDir.toPath(), null, false);
                         if (project == null) continue;
 
-                        loadModules(projectDir, project);
-                        setupJdk(project, pathToJdk);
+                        IntellijProjectUtils.loadModules(projectDir, project);
+                        IntellijProjectUtils.setupJdk(project, pathToJdk);
                         GlobalState.project = project;
 
+                        // Just for test: the types for Type Change should be specified / injected from outside
                         final String sourceType = "java.io.File";
                         final String targetType = "java.nio.file.Path";
 
+                        // Traverse all the `.java` files in the project and build PSI for each of them
                         VirtualFile[] roots = ProjectRootManager.getInstance(project).getContentRoots();
                         for (var root : roots) {
                             VfsUtilCore.iterateChildrenRecursively(root, null, fileOrDir -> {
@@ -89,36 +72,30 @@ public class EvaluationRunner implements ApplicationStarter {
                                 final var psi = PsiManager.getInstance(project).findFile(fileOrDir);
                                 if (psi == null) return true;
 
+                                // Find the first `<caret>` in the program and repair the corresponding element:
+                                // Like `F<caret>ile` to `File`
+                                // TODO: check the next <caret> tags as well
                                 String source = psi.getText();
                                 int caretOffset = source.indexOf("<caret>");
                                 if (caretOffset == -1) return true;
-
-                                PsiElement targetElement = psi.findElementAt(caretOffset);
-                                for (int i = 0; i < 4; ++i) {
-                                    targetElement = Objects.requireNonNull(targetElement).getParent();
-                                }
-                                PsiTypeCodeFragment sourcePsiType = JavaCodeFragmentFactory.getInstance(project)
-                                        .createTypeCodeFragment(sourceType, targetElement, true);
-                                PsiTypeElement sourceTypeElement = JavaPsiFacade.getElementFactory(project)
-                                        .createTypeElement(Objects.requireNonNull(PsiRelatedUtils.getTypeOfCodeFragment(sourcePsiType)));
-                                PsiElement finalTargetElement = targetElement;
-                                WriteCommandAction.runWriteCommandAction(project, () -> {
-                                    finalTargetElement.replace(sourceTypeElement);
-                                });
+                                repairElementWithCaretTag(project, sourceType, psi, caretOffset);
 
                                 WriteCommandAction.runWriteCommandAction(project, () -> {
+                                    // Create built-in `TypeMigrationProcessor`
                                     final PsiElement context = psi.findElementAt(caretOffset);
                                     final var descriptor = TypeChangeRulesStorage.findPattern(sourceType, targetType);
                                     final var typeChangeProcessor = new TypeChangeProcessor(project, false);
                                     final var builtInProcessor = typeChangeProcessor.createBuiltInTypeMigrationProcessor(context, descriptor);
                                     if (builtInProcessor == null) return;
 
+                                    // Disable plugin's internal usages collectors and find migrating usages in the all project
                                     final var typeChangesCollector = TypeChangesInfoCollector.getInstance();
                                     final var requiredImportsCollector = RequiredImportsCollector.getInstance();
                                     typeChangesCollector.off();
                                     requiredImportsCollector.off();
                                     final UsageInfo[] usages = builtInProcessor.findUsages();
 
+                                    // Traverse the usages, extract PsiElements and parents
                                     for (var usage : usages) {
                                         PsiElement element = usage.getElement();
                                         assert element != null;
@@ -146,16 +123,19 @@ public class EvaluationRunner implements ApplicationStarter {
         }
     }
 
-    private void loadModules(File projectDir, Project project) {
-        final var moduleType = JavaModuleType.getModuleType();
-        Path moduleSrcPath = projectDir.toPath().resolve("src");
-        Path moduleImlPath = projectDir.toPath().resolve(".iml");
-        Module module = ModuleManager.getInstance(project).newModule(moduleImlPath, moduleType.getId());
-        PsiTestUtil.addSourceContentToRoots(
-                module,
-                Objects.requireNonNull(LocalFileSystem.getInstance().findFileByPath(moduleSrcPath.toString())),
-                false
-        );
+    private void repairElementWithCaretTag(Project project, String sourceType, PsiFile psi, int caretOffset) {
+        PsiElement targetElement = psi.findElementAt(caretOffset);
+        for (int i = 0; i < 4; ++i) {
+            targetElement = Objects.requireNonNull(targetElement).getParent();
+        }
+        PsiTypeCodeFragment sourcePsiType = JavaCodeFragmentFactory.getInstance(project)
+                .createTypeCodeFragment(sourceType, targetElement, true);
+        PsiTypeElement sourceTypeElement = JavaPsiFacade.getElementFactory(project)
+                .createTypeElement(Objects.requireNonNull(PsiRelatedUtils.getTypeOfCodeFragment(sourcePsiType)));
+        PsiElement finalTargetElement = targetElement;
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            finalTargetElement.replace(sourceTypeElement);
+        });
     }
 
     private CommandLine parseArgs(String[] args) {
