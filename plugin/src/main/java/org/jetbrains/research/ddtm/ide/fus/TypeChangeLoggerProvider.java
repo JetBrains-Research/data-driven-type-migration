e68eb3f7030bc1d15a06d0d718ba879d2b9b22fd
Oleg Smirnov <sm-oleg1209@yandex.ru>
2021-07-22 21:30:23 +0700

    Fix LoggerProvider

--- plugin/src/main/java/org/jetbrains/research/ddtm/ide/fus/TypeChangeLoggerProvider.java
+++ plugin/src/main/java/org/jetbrains/research/ddtm/ide/fus/TypeChangeLoggerProvider.java
@@ -3,20 +3,18 @@ package org.jetbrains.research.ddtm.ide.fus;
 import com.intellij.internal.statistic.eventLog.StatisticsEventLoggerProvider;
 import com.intellij.internal.statistic.utils.StatisticsUploadAssistant;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.registry.Registry;
 
 import java.util.concurrent.TimeUnit;
 
 public class TypeChangeLoggerProvider extends StatisticsEventLoggerProvider {
     public TypeChangeLoggerProvider() {
-        super("DBP", 1, TimeUnit.HOURS.toMillis(1), "200KB");
+        super("DBP", 1, TimeUnit.MINUTES.toMillis(5), "50KB");
     }
 
     @Override
     public boolean isRecordEnabled() {
         return !ApplicationManager.getApplication().isUnitTestMode() &&
                 !ApplicationManager.getApplication().isHeadlessEnvironment() &&
-                Registry.is("feature.usage.event.log.collect.and.upload") &&
                 StatisticsUploadAssistant.isCollectAllowed();
     }
 
