51b86d6b040e21725c3434d7922d5af4038824e9
Oleg Smirnov <sm-oleg1209@yandex.ru>
2021-08-12 14:56:04 +0700

    Add some heuristics to prevent useless suggestions

--- plugin/src/main/java/org/jetbrains/research/ddtm/data/specifications/SourceTypeSpecification.java
+++ plugin/src/main/java/org/jetbrains/research/ddtm/data/specifications/SourceTypeSpecification.java
@@ -15,6 +15,11 @@ public class SourceTypeSpecification extends AbstractPatternSpecification<TypeCh
 
     @Override
     public boolean test(TypeChangePatternDescriptor pattern) {
+        // Heuristics to prevent suggestions like `Migrate to Optional<Optional<Integer>>`, etc.
+        if (sourceType.startsWith("java.util.List") && pattern.getTargetType().startsWith("java.util.List"))
+            return false;
+        if (sourceType.startsWith("java.util.Optional") && pattern.getTargetType().startsWith("java.util.Optional"))
+            return false;
         return SSRUtils.hasMatch(sourceType, pattern.getSourceType(), project);
     }
 }
